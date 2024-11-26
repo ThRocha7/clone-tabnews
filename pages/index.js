6a340b4c9f85b08050752cb81b43afadf01dbec1
Thiago Rocha <102547161+ThRocha7@users.noreply.github.com>
2024-11-26 14:54:06 +0000

    Adicionando a pasta pages

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+function Home() {
+    return <body>
+        <h1>
+            Olá, pessoa!
+        </h1>
+        <p>
+            Saiba que eu te amo S2!
+        </p>
+        
+    </body>;
+}
+
+export default Home;
