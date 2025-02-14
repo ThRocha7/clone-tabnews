d196f34cfe2cb0a56f8972ce12849209f2d5e263
Thiago Rocha <102547161+ThRocha7@users.noreply.github.com>
2025-02-14 13:12:55 +0000

    finish `api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,32 @@
 import database from "infra/database";
+import { version } from "react";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1+1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ message: "deu bom fião" });
+  const updateAt = new Date().toISOString();
+
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnResult = await database.query("SHOW max_connections;");
+  const databaseMaxConnValue = databaseMaxConnResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnResult = await database.query({
+    text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1",
+    values: [databaseName],
+  });
+  const databaseOpenedConnValue = databaseOpenedConnResult.rows[0].count;
+
+  response.status(200).json({
+    update_at: updateAt,
+    dependecies: {
+      database: {
+        max_connections: parseInt(databaseMaxConnValue),
+        opened_connections: databaseOpenedConnValue,
+        version: databaseVersionValue,
+      },
+    },
+  });
 }
 
 export default status;
