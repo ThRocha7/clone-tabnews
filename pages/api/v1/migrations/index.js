1c2baf3db517c69597f12ae57d8cb79cb4f2bb6c
Thiago Rocha <102547161+ThRocha7@users.noreply.github.com>
2025-03-05 15:36:31 +0000

    fix: code `/migrations` endpoint

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -3,11 +3,11 @@ import { join } from "node:path";
 import database from "infra/database";
 
 export default async function migrations(request, response) {
-  console.log(request.method);
-  if (request.method !== "POST" && request.method !== "GET") {
-    return response
-      .status(405)
-      .json({ message: `The method ${request.method} is not allowed` });
+  const allowedMethods = ["GET", "POST"];
+  if (!allowedMethods.includes(request.method)) {
+    return response.status(405).json({
+      error: `Method "${request.method}" not allowed`,
+    });
   }
 
   let dbClient;
