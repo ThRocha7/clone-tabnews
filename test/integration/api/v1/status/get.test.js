da5d9c3db3d9279bf09dfa0c7d3dfc1afe6b8177
Thiago Rocha <102547161+ThRocha7@users.noreply.github.com>
2025-03-05 14:44:58 +0000

    letting `api/v1/status` the code more polish

--- test/integration/api/v1/status/get.test.js
+++ test/integration/api/v1/status/get.test.js
@@ -3,12 +3,10 @@ test("GET to api/v1/status should return 200", async () => {
   expect(response.status).toBe(200);
 
   const responseBody = await response.json();
+  const parsingDate = new Date(responseBody.update_at).toISOString();
 
   expect(responseBody.update_at).toBeDefined();
-
-  const parsingDate = new Date(responseBody.update_at).toISOString();
   expect(responseBody.update_at).toEqual(parsingDate);
-
   expect(responseBody.dependecies.database.version).toBe("16.0");
   expect(responseBody.dependecies.database.max_connections).toBe(100);
   expect(responseBody.dependecies.database.opened_connections).toBe(1);
