be1d806e925f73c8f696a1142c419a0b650485b8
Thiago Rocha <102547161+ThRocha7@users.noreply.github.com>
2025-03-09 01:46:25 +0000

    adds Automated Tests GitHub Action

--- test/orchestrator.js
+++ test/orchestrator.js
@@ -1,16 +1,19 @@
 import retry from "async-retry";
 
 export default async function waitForAllServices() {
-  return retry(
-    async () => {
-      const response = await fetch("http://localhost:3000/api/v1/status");
-      if (response.status !== 200) {
-        throw Error();
-      }
-    },
-    {
+  await waitForWebServer();
+
+  async function waitForWebServer() {
+    return retry(fetchStatusPage, {
       retries: 100,
       maxTimeout: 1000,
-    },
-  );
+    });
+  }
+
+  async function fetchStatusPage() {
+    const response = await fetch("http://localhost:3000/api/v1/status");
+    if (response.status !== 200) {
+      throw Error();
+    }
+  }
 }
